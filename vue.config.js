f0c55833280937c07c242e182d7f68a8ed49b385
Sheyh <965598930@qq.com>
2022-08-29 09:51:05 +0800

    备份一下

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
     //打包是否生成.map文件
     // publicPath: process.env.NODE_ENV === 'production' ? "./../" : './',//服务器用
     // assetsDir: "static",
+    publicPath:'./',
     productionSourceMap: false,
     lintOnSave: false,
 };
